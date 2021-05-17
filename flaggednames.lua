8db01e564c8f562e2d31090cc68b6176e9b8225a
LolloDev <andreapascullita@Gmail.com>
2021-05-17 23:21:22 +0200

    Create flaggednames.lua

---
+++ flaggednames.lua
@@ -0,0 +1,19 @@
+names = {
+  "23k",
+  "22k",
+  "25k",
+  "27k",
+  "29k",
+  "I_",
+  "Girl",
+  "Itz",
+  "It_",
+  "beau",
+  "bella",
+  "Yara",
+  "Zoe",
+  "Chloe",
+  "Ashley"
+}
+
+return names
