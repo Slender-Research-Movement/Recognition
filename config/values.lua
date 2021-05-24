f311e904f1593b72de7ac3b3eba62f7afc9a1f64
LolloDev <andreapascullita@Gmail.com>
2021-05-25 00:52:56 +0200

    Create values.lua

---
+++ config/values.lua
@@ -0,0 +1,8 @@
+local values = {}
+
+-- Flag Points when flagged
+values.FLAG_NAME = 0.5 -- Flagged depending on specific part inside username
+values.FLAG_ITEM = 1   -- Flagged depending on possesed item
+values.FLAG_GAME = 0.1 -- Flagged depending on game
+
+return values
