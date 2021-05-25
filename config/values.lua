20c0f4638da999b18eff30ae63890f8044cb4908
LolloDev <andreapascullita@Gmail.com>
2021-05-25 22:17:46 +0200

    Update values.lua

--- config/values.lua
+++ config/values.lua
@@ -1,8 +1,8 @@
 local values = {}
 
--- Flag Points when flagged
-values.FLAG_NAME = .5 -- Flagged depending on specific part inside username
-values.FLAG_ITEM = 1   -- Flagged depending on possesed item
-values.FLAG_GAME = .1 -- Flagged depending on game
+--#@> Flag Points when flagged
+values.FLAG_NAME = .5 --#@> Flagged depending on specific part inside username
+values.FLAG_ITEM = 1  --#@> Flagged depending on possesed item
+values.FLAG_GAME = .1 --#@> Flagged depending on game
 
 return values
