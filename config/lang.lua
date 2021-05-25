03c04683c5d6d23fff25fc72ee84ac2c73197e4f
LolloDev <andreapascullita@Gmail.com>
2021-05-25 22:00:14 +0200

    Update lang.lua

--- config/lang.lua
+++ config/lang.lua
@@ -1,10 +1,40 @@
 local lang = {}
 
+lang.Template = {
+Header = "⚜️" --@#> [STRING] Log Emoji Header
+Title = "Template for Pull Requests" --@#> [STRING] Log Title
+--@#> [STRING] Log Inner -> Is to be provided by script
+Importance = 2 --@#> [INT] Describes how many spaces after a '\n'
+  
+--@#> ======== EXAMPLE PRINT ========
+--@#>  |⚜️| Template for Pull Requests | nil
+}
+
+
 lang.READY = ""
 
-lang.APPEARENCE_ERROR = "|🛑| Cannot get appearence info |"
-lang.FLAGGED_ITEM = "|🚩| Flagged Item |"
-lang.FLAGGED_NAME = "|🏁| Flagged Name |"
-lang.DETECTED = "|💽| Detected Slender |"
+lang.APPEARENCE_ERROR = {
+Header = "🛑"
+Title = "Cannot get appearence info"
+Importance = 0
+}
+
+lang.FLAGGED_ITEM = {
+Header = "🚩"
+Title = "Flagged Item"
+Importance = 3
+}
+
+lang.FLAGGED_NAME = {
+Header = "🏁"
+Title = "Flagged Name"
+Importance = 4
+}
+
+lang.DETECTED = {
+Header = "💽"
+Title = "Detected Slender"
+Importance = 0
+}
 
 return lang
